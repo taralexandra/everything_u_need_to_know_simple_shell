01b6ffe10a8d725c4db600c7850f043a88291fdd
taralexandra <taralexandra@gmail.com>
2024-08-13 16:09:44 +0200

    Adding and updating 0-printenv.c and 1-env_vs_environ.c

---
+++ 1-env_vs_environ.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+extern char **environ;
+
+int main(int ac, char **av, char **env)
+{
+
+	printf("Address of env: %p\n", (void *)env);
+
+	printf("Address of environ: %p\n", (void *)environ);
+
+	return (0);
+
+}
